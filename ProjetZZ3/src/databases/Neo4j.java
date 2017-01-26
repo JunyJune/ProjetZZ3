30162282880bb23f72bb0df7cac7fceb04e4d39e
Marvin <Marvin@Muffin-pc>
2017-01-26 15:52:41 +0100

    Ajout fonction qui recupere le resultat dans Mongo et ajout Operations CRUD Mongo

--- ProjetZZ3/src/databases/Neo4j.java
+++ ProjetZZ3/src/databases/Neo4j.java
@@ -1,125 +1,194 @@
-//package databases;
-//
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//
-//import org.neo4j.driver.v1.AuthTokens;
-//import org.neo4j.driver.v1.Config;
-//import org.neo4j.driver.v1.Driver;
-//import org.neo4j.driver.v1.GraphDatabase;
-//import org.neo4j.driver.v1.Session;
-//import org.neo4j.graphdb.GraphDatabaseService;
-//import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-//
-//public class Neo4j {
-//	private String filePath;
-//	private static String DB_PATH = "C:/Users/Aude/Desktop/ZZ3/ProjetZZ3/Databases";
-//	private File DB_FILE = new File(DB_PATH);
-//	
-//	private GraphDatabaseService graphDb;
-//	//private Session session;
-//	private Driver driver;
-//	/******************************************************************************************/
-//	/***************************************Accesseurs*****************************************/
-//	/******************************************************************************************/
-//	
-//	public void setFilePath(String filePath){
-//		this.filePath = filePath;
-//	}
-//	
-//	public String getFilePath(){
-//		return filePath;
-//	}
-//	
-//	/******************************************************************************************/
-//	/****************************************Fonctions*****************************************/
-//	/******************************************************************************************/	
-//
-//	
-//	/*private static void registerShutdownHook(final GraphDatabaseService graphDb){
-//		Runtime.getRuntime().addShutdownHook(
-//				new Thread(){
-//					@Override
-//					public void run(){
-//						graphDb.shutdown();
-//					}
-//				}
-//		);
-//	}*/
-//	
-//	public void Connect(){
-//		System.out.println("Connexion à la base de données Neo4j");
-//		
-//		/*GraphDatabaseFactory generateur = new GraphDatabaseFactory();
-//		graphDb = generateur.newEmbeddedDatabase(DB_FILE);
-//		registerShutdownHook(graphDb);*/
-//		driver = GraphDatabase.driver( "bolt://localhost", AuthTokens.basic("neo4j", "46wjxxpc"), Config.build()
-//		        .withEncryptionLevel( Config.EncryptionLevel.REQUIRED )
-//		        .withTrustStrategy( Config.TrustStrategy.trustOnFirstUse( new File( "/path/to/neo4j_known_hosts" ) ) )
-//		        .toConfig() );
-//		 
-//	}
-//	
-//	public void Insert(){
-//		System.out.println("Insertion dans la base de données Neo4j");
-//		
-//		Session session = driver.session();
-//		//ouverture du fichier destiné à NEO4J
-//		//String fichier ="DataFiles/data.txt";
-//		System.out.println("Debut de l'insertion");
-//		//lecture du fichier texte	
-//		try{
-//			InputStream ips=new FileInputStream(getFilePath()); 
-//			InputStreamReader ipsr=new InputStreamReader(ips);
-//			BufferedReader br=new BufferedReader(ipsr);
-//			String ligne;	
-//			String chaine="";
-//			int inc=0;
-//			long startTime = System.currentTimeMillis();
-//
-//			while ((ligne=br.readLine())!=null){
-//				if(ligne.length() != 0){
-//					 //on recupère ici les 5 relations de creation des tables et relations
-//						chaine +=ligne;
-//						inc ++;
-//						if(inc%5 == 0){
-//						session.run( chaine );
-//						chaine = "";
-//						}
-//
-//				}
-//			}
-//			long endTime = System.currentTimeMillis();
-//			br.close();
-//			System.out.println("Insertion effectuée");
-//		    System.out.println("Temps total d'executiion de l'insertion :"+ (endTime-startTime) +"ms");
-// 
-//			
-//		}		
-//		catch (Exception e){
-//			System.out.println(e.toString());
-//		}
-//		session.close();
-//	}
-//
-//	
-//	public void Update(){
-//		System.out.println("Mise à jour dans la base de données Neo4j");
-//	}
-//	
-//	public void Read(){
-//		System.out.println("Lecture de la base de données Neo4j");
-//	}
-//	
-//	public void Delete(){
-//		System.out.println("Suppresion dans la base de données Neo4j");
-//	}
-//	
-//	public void Disconnect(){
-//		//graphDb.shutdown();
-//		driver.close();
-//	}
-//}
+package databases;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+
+public class Neo4j {
+	private String filePath;
+	//private static String DB_PATH = "C:/Users/Aude/Desktop/ZZ3/ProjetZZ3/Databases";
+	//private File DB_FILE = new File(DB_PATH);
+	
+	//private GraphDatabaseService graphDb;
+	//private Session session;
+	private Driver driver;
+	private Session session;
+	/******************************************************************************************/
+	/***************************************Accesseurs*****************************************/
+	/******************************************************************************************/
+	
+	public void setFilePath(String filePath){
+		this.filePath = filePath;
+	}
+	
+	public String getFilePath(){
+		return filePath;
+	}
+	
+	@Override
+	public String toString(){
+		String name = "NEO4J";
+		return name;
+	}
+	
+	/******************************************************************************************/
+	/****************************************Fonctions*****************************************/
+	/******************************************************************************************/	
+
+	
+	/*private static void registerShutdownHook(final GraphDatabaseService graphDb){
+		Runtime.getRuntime().addShutdownHook(
+				new Thread(){
+					@Override
+					public void run(){
+						graphDb.shutdown();
+					}
+				}
+		);
+	}*/
+	
+	public void Connect(){
+		System.out.println("Connexion à la base de données Neo4j");
+		
+		/*GraphDatabaseFactory generateur = new GraphDatabaseFactory();
+		graphDb = generateur.newEmbeddedDatabase(DB_FILE);
+		registerShutdownHook(graphDb);*/
+		driver = GraphDatabase.driver( "bolt://localhost", AuthTokens.basic("neo4j", "46wjxxpc"), Config.build()
+		        .withEncryptionLevel( Config.EncryptionLevel.REQUIRED )
+		        .withTrustStrategy( Config.TrustStrategy.trustOnFirstUse( new File( "/path/to/neo4j_known_hosts" ) ) )
+		        .toConfig() );
+		 session = driver.session();
+		 
+	}
+	
+	public void Insert(){
+		System.out.println("Insertion dans la base de données Neo4j");
+		
+		
+		//ouverture du fichier destiné à NEO4J
+		//String fichier ="DataFiles/data.txt";
+		System.out.println("Debut de l'insertion");
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(getFilePath()); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;	
+			String chaine="";
+			int inc=0;
+			long startTime = System.currentTimeMillis();
+
+			while ((ligne=br.readLine())!=null){
+				if(ligne.length() != 0){
+					 //on recupère ici les 5 relations de creation des tables et relations
+						chaine +=ligne;
+						inc ++;
+						if(inc%5 == 0){
+						session.run( chaine );
+						chaine = "";
+						}
+
+				}
+			}
+			long endTime = System.currentTimeMillis();
+			br.close();
+			System.out.println("Insertion effectuée");
+		    System.out.println("Temps total d'executiion de l'insertion :"+ (endTime-startTime) +"ms");
+ 
+			
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+		
+	}
+
+	
+	public void Update(){
+		System.out.println("Mise à jour dans la base de données Neo4j");
+		long startTime = System.currentTimeMillis();
+		StatementResult result = session.run( "MATCH (a:Client) WHERE a.abonnement_client = 'false' " +
+				"SET a.abonnement_client = 'true' " +
+                "RETURN a.id_client AS abo" );
+		long endTime = System.currentTimeMillis();
+		System.out.println("Temps total d'executiion de la mise à jour :"+ (endTime-startTime) +"ms");
+		//Affichage des noeuds modifiés
+		while ( result.hasNext() )
+		{
+			Record record = result.next();
+			System.out.println( record.get( "abo" ).asString() );
+			
+		}
+		
+	}
+	
+	public void Read(){
+		System.out.println("Lecture de la base de données Neo4j");
+		long startTime = System.currentTimeMillis();
+		
+		StatementResult result = session.run( "MATCH p=(n)-[r]->(m) " +
+				"RETURN keys(n),keys(r),keys(m) " );
+		//OU//
+		/*StatementResult result = session.run( "MATCH p=(n)-[r]->(m) " +
+				"RETURN * " );*/
+		
+		/*StatementResult result = session.run( "MATCH (cl:Client)--(c:Commande)--(p:Produit)--(f:Fournisseur) " +
+				"RETURN cl.id_client,c.id_commande,p.id_produit,f.id_fournisseur " );*/
+		while ( result.hasNext() )
+		{
+			Record record = result.next();
+			System.out.println( record );
+			
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Temps total d'executiion de la lecture :"+ (endTime-startTime) +"ms");
+	}
+	
+	public void Delete(){
+		System.out.println("Suppresion dans la base de données Neo4j");
+		
+		System.out.println("Mise à jour dans la base de données Neo4j");
+		long startTime = System.currentTimeMillis();
+		
+		
+		/****************************Cas suppression client -> suppresion relation commande/client et supp commande *****************************/
+		/* session.run( "MATCH (a:Client)--(commande:Commande) WHERE a.id_client = '3' " +
+				"DETACH DELETE commande, a" );*/
+		
+		
+		/***************************Cas suppresion commande -> suppresion relation commande/produit, commande/client************/
+		/*session.run( "MATCH (a:Commande) WHERE a.id_commande = '4' " +
+				"DETACH DELETE a" );*/
+		
+		/*********Cas suppression produit -> suppression relation commande/produit, produit/fournisseur, suppresion fournisseur et commande***********/
+		/*session.run( "MATCH (fournisseur:Fournisseur)--(a:Produit)--(commande:Commande) WHERE a.id_produit = '900000005' " +
+				"DETACH DELETE commande, fournisseur, a" );*/
+		
+		
+		/*********Cas suppression fournisseur -> suppression produit, commande, et relation fournisseur/produit, produit/commande, commande/client***********/
+		/*session.run( "MATCH (a:Fournisseur)--(produit:Produit)--(commande:Commande) WHERE a.id_fournisseur = '4971' " +
+				"DETACH DELETE commande, produit, a" );*/
+		
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("Temps total d'executiion de la suppresion :"+ (endTime-startTime) +"ms");
+		
+		
+	}
+	
+	public void Disconnect(){
+		//graphDb.shutdown();
+		driver.close();
+		session.close();
+	}
+}
--- ProjetZZ3/src/main/Main.java
+++ ProjetZZ3/src/main/Main.java
@@ -7,6 +7,7 @@ import com.mongodb.DBCollection;
 
 import databases.MongoDB;
 import databases.MySQL;
+import databases.Neo4j;
 //import databases.Neo4j;
 import databases.OracleNoSQL;
 import files.CSVtoJSON;
@@ -29,41 +30,50 @@ public class Main {
 		CSVtoNEO neoConverter = new CSVtoNEO();
 		
 		try {
-			jsonConverter.convertCSVtoJSON(data_filePath_Source);
-			sqlConverter.execute(data_filePath_Source);
+			//jsonConverter.convertCSVtoJSON(data_filePath_Source);
+			//sqlConverter.execute(data_filePath_Source);
 			neoConverter.execute(data_filePath_Source);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 				
-//		MongoDB mongoDB = new MongoDB();
-//		mongoDB.setFilePath(data_filePath_Json);
-//		mongoDB.InitialisationListes();
-//		mongoDB.Connect();
-//		mongoDB.Insert("SecondCollection", "FirstDocument");
-//		mongoDB.InsertFile();
+		MongoDB mongoDB = new MongoDB();
+		mongoDB.setFilePath(data_filePath_Json);
+		mongoDB.InitialisationListes();
+		mongoDB.Connect();
+//		mongoDB.Insert();
 //		mongoDB.ReadAllDatabase();
-//		mongoDB.Disconnect();
+//		mongoDB.Update();
+//		mongoDB.UpdateWithParameter("id_client", "6", "abonnement_client", "false");
+//		mongoDB.DeleteOne("id_client", "10");
+//		mongoDB.DeleteAll();
+		mongoDB.DeleteBetween("id_fournisseur", "2000", "4000");
+		mongoDB.Disconnect();
 		
-		MySQL mySQL = new MySQL();
-		mySQL.setFilePath(data_filePath_Sql);
-		mySQL.Connect();
+		
+//		MySQL mySQL = new MySQL();
+//		mySQL.setFilePath(data_filePath_Sql);
+//		mySQL.Connect();
 //		mySQL.DeleteClient();
 //		mySQL.DeleteFournisseur();
-		mySQL.DeleteProduit();
+//		mySQL.DeleteProduit();
 //		mySQL.DeleteCommande();
 //		mySQL.Delete();
 //		mySQL.DropTable();
 //		mySQL.CreateTables();
 //		mySQL.Insert();
-		mySQL.Read();
-		mySQL.Disconnect();
+//		mySQL.Read();
+//		mySQL.Disconnect();
 		
-		/*Neo4j neo4j = new Neo4j();
+		Neo4j neo4j = new Neo4j();
 		neo4j.setFilePath(data_filePath_Neo);
 		neo4j.Connect();
 		neo4j.Insert();
-		neo4j.Disconnect();*/
+		//neo4j.Update();
+		//neo4j.Delete();
+		//neo4j.Read();
+		neo4j.Disconnect();
+
 		
 //		OracleNoSQL oracleNoSQL = new OracleNoSQL();
 //		oracleNoSQL.setFilePath(data_filePath_Source);
